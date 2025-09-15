a1218ebbeb02153ac81db3dd7cf0cc4341d9daf0
s1120198-cyber <s1120198@dmhs.kh.edu.tw>
2025-09-15 03:30:56 +0000

    第一次提交

---
+++ test1/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+int main(){
+int main 
+ {
+ {
+    std::cout << "Fair\n";
+/  } else {
+    std::cout << "Keep going!\n";
+ }
+ return 0;
+  {
+  std::string name;    // 姓名
+  int score = 0;       // 分數
+  std::cout << "Name and score: ";
+  std::cin >> name >> score; // demo 簡單起見用空白分隔（進階可改 getline）
+  std::cout << name << " got " << score << " points.\n";
+  小延伸：if (score >= 60) 印 "Pass" 否則 "Fail"
+ return 0;
+}git add main.cpp
+git commit -m "解鎖第一段 main"
